c32ad7940ff976aee9f624e10745f6d5056b03f7
lukechu10 <37006668+lukechu10@users.noreply.github.com>
2023-05-03 22:51:46 +0100

    add back default keybinds for nvim-tree

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -96,11 +96,12 @@ return {
 					api.tree.reload()
 				end
 
+				api.config.mappings.default_on_attach(bufnr)
+
 				vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
 				vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
 				vim.keymap.set("n", "h", api.tree.close, opts("Close"))
-				vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
-				vim.keymap.set('n', 'ga', git_add, opts('Git Add'))
+				vim.keymap.set('n', 'a', git_add, opts('Git Add'))
 			end
 
 			require("nvim-tree").setup({
