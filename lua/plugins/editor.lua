9b2af40ba90117d6200bae2d397e7c8e1809a696
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-06-22 20:38:08 +0100

    Add config for vim-vsnip

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -125,6 +125,15 @@ return {
 	},
 	{
 		"hrsh7th/vim-vsnip",
+		config = function()
+			vim.cmd [[
+				" Jump forward or backward
+				imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+				smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+				imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+				smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+			]]
+		end
 	},
 
 	{
