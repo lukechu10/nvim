b0464834b5113928c1e3273c030de71904fec42d
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-08-03 22:34:43 -0700

    Add DAP configuration

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,93 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local wk = require("which-key")
+			wk.register({
+				["<leader>"] = {
+					d = {
+						name = "debug",
+						s = {
+							name = "step",
+							c = { "<cmd>lua require('dap').continue()<CR>", "Continue" },
+							v = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
+							i = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
+							o = { "<cmd>lua require('dap').step_out()<CR>", "Step Out" },
+						},
+						b = {
+							name = "breakpoints",
+							t = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Toggle Breakpoint" },
+							c = { "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+								"Set Conditional Breakpoint" },
+							m = {
+								"<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+								"Set Log Point" },
+						}
+					}
+				}
+			})
+
+			local dap = require("dap")
+
+			local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+			local codelldb_path = mason_path .. "bin/codelldb"
+			local this_os = vim.loop.os_uname().sysname
+
+			-- The path in windows is different
+			if this_os:find "Windows" then
+				codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+			end
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = codelldb_path,
+					args = { "--port", "${port}" },
+				}
+			}
+			dap.adapters.lldb = dap.adapters.codelldb
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+				},
+			}
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
+
+			require("dap.ext.vscode").load_launchjs(nil, { lldb = { "c", "cpp", "rust" } })
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("nvim-dap-virtual-text").setup()
+		end
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end
+	}
+}
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -114,7 +114,26 @@ return {
 		event = "VeryLazy",
 		config = function()
 			local rt = require("rust-tools")
+
+			local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+
+			local codelldb_path = mason_path .. "bin/codelldb"
+			local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
+			local this_os = vim.loop.os_uname().sysname
+
+			-- The path in windows is different
+			if this_os:find "Windows" then
+				codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+				liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
+			else
+				-- The liblldb extension is .so for linux and .dylib for macOS
+				liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+			end
+
 			rt.setup({
+				dap = {
+					adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
 				server = {
 					on_attach = function(_, buffnr)
 						on_attach()
