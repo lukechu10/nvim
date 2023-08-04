cd2808d81a0078155e1908720020b55b42c7ddee
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-08-03 18:22:36 -0700

    Add C-j and C-k to navigate completion and use CR for accept

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -64,6 +64,8 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+					["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<Tab>"] = cmp.mapping(function(fallback)
@@ -78,6 +80,13 @@ return {
 							fallback()
 						end
 					end, { "i", "s", "c" }),
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() and cmp.get_active_entry() then
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+						else
+							fallback()
+						end
+					end, { "i" }),
 				}),
 				sources = cmp.config.sources({
 						{ name = "nvim_lsp" },
