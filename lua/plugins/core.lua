f1bbb90ebcac8b4bb0cb61d9f50c3279f8a8058b
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-09-22 10:50:53 +0200

    Remove noice

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,28 +1,11 @@
 return {
 	{
-		"smjonas/inc-rename.nvim",
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		event = "LspAttach",
 		config = function()
-			require("inc_rename").setup()
+			require("fidget").setup()
 		end,
-	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			presets = {
-				bottom_search = true,
-				inc_rename = true,
-				lsp_doc_border = true,
-			}
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		}
 	},
 	"tpope/vim-repeat",
 }
