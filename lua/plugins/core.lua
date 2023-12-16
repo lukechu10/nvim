0efcd25f62dbad840dc1e69526fadd3d408d11f9
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-12-16 11:41:18 -0800

    Fix gf, gF in FTerm

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -15,9 +15,28 @@ return {
 			{ "<A-i>", "<cmd>lua require('FTerm').toggle()<cr>",            desc = "Toggle FTerm" },
 			{ "<A-i>", "<C-\\><C-n><cmd>lua require('FTerm').toggle()<cr>", mode = "t",           desc = "Toggle FTerm" },
 		},
-		opts = {
-			auto_close = false,
-			cmd = vim.opt.shell._value,
-		}
+		config = function()
+			function _G.open_in_normal_window(cmd)
+				local cursor = vim.api.nvim_win_get_cursor(0)
+				local bufnr = vim.api.nvim_get_current_buf()
+				local f = vim.fn.findfile(vim.fn.expand("<cfile>"))
+				if f ~= "" and vim.api.nvim_win_get_config(vim.fn.win_getid()).anchor ~= "" then
+					require("FTerm").close()
+					vim.api.nvim_win_set_buf(0, bufnr)
+					vim.api.nvim_win_set_cursor(0, cursor)
+					vim.cmd("normal! " .. cmd)
+				end
+			end
+
+			vim.cmd [[
+				autocmd FileType FTerm nnoremap <silent><buffer> gf <cmd>lua open_in_normal_window("gf")<cr>
+				autocmd FileType FTerm nnoremap <silent><buffer> gF <cmd>lua open_in_normal_window("gF")<cr>
+			]]
+
+			require("FTerm").setup({
+				auto_close = false,
+				cmd = vim.opt.shell._value,
+			})
+		end,
 	}
 }
