6a0e19fc6a330b56d015c4a30395d1c2c9bcd8a0
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-03-20 21:17:32 -0700

    Add some more treesitter keybinds

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -22,17 +22,36 @@ return {
 						enable = true,
 						lookahead = true,
 						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
 							["af"] = "@function.outer",
 							["if"] = "@function.inner",
+
 							["ac"] = "@class.outer",
-							-- You can optionally set descriptions to the mappings (used in the desc parameter of
-							-- nvim_buf_set_keymap) which plugins like which-key display
-							["ic"] = {
-								query = "@class.inner",
-								desc =
-								"Select inner part of a class region"
-							},
+							["ic"] = "@class.inner",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
 						},
 					},
 					include_surrounding_whitespace = true,
