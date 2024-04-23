fed3f8db67617ef813fa9116ca8a6a60149d964d
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-04-23 08:53:00 +0100

    Configure vimtex filetype

--- lua/plugins/extras/lang/tex.lua
+++ lua/plugins/extras/lang/tex.lua
@@ -2,10 +2,12 @@ return {
 	{
 		"lervag/vimtex",
 		tag = "v2.15",
+		ft = { "tex" },
 		config = function()
 			vim.cmd [[
 				filetype plugin indent on
 				let g:vimtex_compiler_latexmk = {'executable' : 'latexmk.exe'}
+				
 			]]
 		end
 	}
