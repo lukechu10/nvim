24ecd4a31ae3723f85849cb0279dd4703c6a1269
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-04-23 11:17:52 +0100

    Add some autocmd for tex

--- lua/plugins/extras/lang/tex.lua
+++ lua/plugins/extras/lang/tex.lua
@@ -7,8 +7,18 @@ return {
 			vim.cmd [[
 				filetype plugin indent on
 				let g:vimtex_compiler_latexmk = {'executable' : 'latexmk.exe'}
-				
 			]]
+
+			-- Enable spell checking and conceal automatically in tex files
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+				pattern = { "*.tex" },
+				group = vim.api.nvim_create_augroup("TexSettings", { clear = true }),
+				callback = function()
+					vim.opt_local.spell = true
+					vim.opt_local.spelllang = "en_us"
+					vim.opt_local.conceallevel = 2
+				end
+			})
 		end
 	}
 }
