c83e4f1f3c4b2a237dbabed113919c21c9723bcd
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-05-27 21:27:29 +0100

    Add wordcount to lualine

--- lua/plugins/statusline/init.lua
+++ lua/plugins/statusline/init.lua
@@ -1,3 +1,19 @@
+-- Count number of words in current buffer for display in lualine.
+local function getWords()
+	local words = vim.fn.wordcount().words
+	if words == 1 then
+		return words .. " word"
+	else
+		return words .. " words"
+	end
+end
+
+-- Check if current buffer is one of text,md,markdown,tex.
+local function isTextBuffer()
+	local ft = vim.bo.filetype
+	return ft == "text" or ft == "md" or ft == "markdown" or ft == "tex"
+end
+
 return {
 	{
 		"nvim-lualine/lualine.nvim",
@@ -8,7 +24,7 @@ return {
 				lualine_a = { 'mode' },
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
 				lualine_c = { 'filename' },
-				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_x = { { getWords, cond = isTextBuffer }, 'encoding', 'fileformat', 'filetype' },
 				lualine_y = { 'progress' },
 				lualine_z = { 'location' }
 			},
