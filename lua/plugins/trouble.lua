2f486543416a3c78f44f3c6698457f3205b59181
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-07-09 08:04:49 -0700

    Update trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,14 @@
 return {
 	"folke/trouble.nvim",
+	opts = {
+		warn_no_results = false,
+		open_no_results = true,
+	},
+	cmd = "Trouble",
 	keys = {
 		{
 			"<leader>x",
-			"<cmd>TroubleToggle<cr>",
+			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Toggle diagnostics list"
 		}
 	}
