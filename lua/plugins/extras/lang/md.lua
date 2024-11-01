870b830b14d284cd6ab91df053d87c5e2e7a25e2
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-11-01 16:16:09 +0000

    Remove markview since too laggy

--- lua/plugins/extras/lang/md.lua
+++ lua/plugins/extras/lang/md.lua
@@ -5,23 +5,4 @@ vim.filetype.add({
 	}
 })
 
-return {
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false, -- Already supported by the plugin.
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			modes = { "n", "i", "no", "c" },
-			hybrid_modes = { "i" },
-			callbacks = {
-				on_enable = function(_, win)
-					vim.wo[win].conceallevel = 2
-					vim.wo[win].concealcursor = "nc"
-				end,
-			}
-		}
-	}
-}
+return {}
