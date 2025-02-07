cb046f78ce21f0b9496649b21285ec511de26118
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-02-07 14:43:18 +0000

    Add lazydev to blink.cmp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -33,7 +33,14 @@ return {
 				["<C-k>"] = { "select_prev", "fallback" },
 			},
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" }
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					}
+				}
 			},
 			completion = {
 				documentation = { auto_show = true },
