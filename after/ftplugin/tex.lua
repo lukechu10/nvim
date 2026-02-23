5e4f9ab08e345f1defd551cbc88d331f0e615442
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2026-02-23 14:25:52 +0000

    feat: disable vim spell checking to not interfere with Harper

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,7 +1,7 @@
-vim.opt_local.spell = true
-vim.opt_local.spelllang = "en_us"
-
--- <C-l> to quickly fix spelling
-vim.cmd [[inoremap <C-l> <c-g>u<Esc>[s1z=`]a<c-g>u]]
+-- vim.opt_local.spell = true
+-- vim.opt_local.spelllang = "en_us"
+--
+-- -- <C-l> to quickly fix spelling
+-- vim.cmd [[inoremap <C-l> <c-g>u<Esc>[s1z=`]a<c-g>u]]
 
 vim.opt_local.conceallevel = 1
